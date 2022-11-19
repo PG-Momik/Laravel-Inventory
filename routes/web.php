c74ba7ffc6556c68dc5f0fddef40b7c02c1a2196
PG-Momik <momik12339@gmail.com>
2022-11-19 17:20:55 +0545

    Smoll super insignificant change in main

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AjaxController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
