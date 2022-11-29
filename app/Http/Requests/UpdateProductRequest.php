ff3f6f6f0392c50e3005bf8c010f0c9d004988c5
PG-Momik <momik12339@gmail.com>
2022-11-29 14:11:13 +0545

    Implemented FormRequest on ProductController - Created and used Requests/CreatProductRequest.php - Created Requests/UpdateProductRequest.php

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name'          => 'required|string',
+            'category'      => 'required|numeric',
+            'quantity'      => 'required|numeric|min:0',
+            'price'         => 'numeric|min:0',
+            'purchasePrice' => 'numeric|min:0',
+            'salesPrice'    => 'numeric|min:0',
+            'discount'      => 'nullable|numeric|min:0',
+            'description'   => 'nullable|string',
+            'productImage'  => 'nullable|image',
+        ];
+    }
+}
